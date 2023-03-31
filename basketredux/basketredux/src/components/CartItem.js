d3dd5fb7d0ab973541f2a06a100293a387860318
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-03-31 23:26:54 +0430

    switch

--- basketredux/basketredux/src/components/CartItem.js
+++ basketredux/basketredux/src/components/CartItem.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { connect } from "react-redux";
+import { DECREASE, INCREASE, REMOVE } from "./actions";
 
-const CartItem = ({ img, title, price, amount }) => {
+const CartItem = ({ img, title, price, amount, remove }) => {
   return (
     <div className="cart-item">
       <img src={img} alt={title} />
@@ -8,7 +10,9 @@ const CartItem = ({ img, title, price, amount }) => {
         <h4>{title}</h4>
         <h4 className="item-price">${price}</h4>
         {/* remove button */}
-        <button className="remove-btn">remove</button>
+        <button className="remove-btn" onClick={() => remove()}>
+          remove
+        </button>
       </div>
       <div>
         {/* increase amount */}
@@ -30,4 +34,7 @@ const CartItem = ({ img, title, price, amount }) => {
   );
 };
 
-export default CartItem;
+const mapDispatchToProps = (dispatch) => {
+  return { remove: () => dispatch({ type: REMOVE }) };
+};
+export default connect(null, mapDispatchToProps)(CartItem);
--- basketredux/basketredux/src/reducer.js
+++ basketredux/basketredux/src/reducer.js
@@ -1,7 +1,32 @@
-import { DECREASE, INCREASE } from "./actions";
+import { DECREASE, INCREASE, CLEAR_CART, REMOVE } from "./actions";
 
 function reducer(state, action) {
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] };
+  }
+  if (action.type === DECREASE) {
+    console.log("you decreased amount!");
+  }
+  if (action.type === INCREASE) {
+    console.log("you increased amount!");
+  }
+  if (action.type === REMOVE) {
+    console.log("you remove amount!");
+  }
   return state;
 }
 
 export default reducer;
+
+//  1
+// if(action.type === CLEAR_CART){
+//     return{...state,cart:[]}
+// }
+// return state;
+//or 2
+// switch(action.type){
+//     case CLEAR_CART:
+//         return{...state,cart:[]};
+//         default:
+//             return state;
+// }
