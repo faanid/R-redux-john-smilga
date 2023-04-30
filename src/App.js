a88f0d74bb8757b21add144d3a65f1fefc9e14d6
FND <73032767+faanid@users.noreply.github.com>
2023-04-30 08:08:34 +0000

    cartitems

--- src/App.js
+++ src/App.js
@@ -3,27 +3,17 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 // items
-
+import cartItems from "./cart-items";
 // redux stuff
-
-import { configureStore } from "redux";
-import reducer from "./reducer";
-import { Provider } from "react-redux";
-//initial store
-
-const store = configureStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 function App() {
   // cart setup
 
   return (
-    <Provider store={store}>
+    <main>
       <Navbar />
-      <CartContainer />
-    </Provider>
+      <CartContainer cart={cartItems} />
+    </main>
   );
 }
 
